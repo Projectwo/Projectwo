bdf9151254fb1bde95c69beb260fa1883fd4ba1b
juneon <tly013@naver.com>
2022-11-19 22:58:16 +0900

    feat: 초기 세팅중

---
+++ src/test/java/com/project/Projectwo/ProjectwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Projectwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
