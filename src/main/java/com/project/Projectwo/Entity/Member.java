cfc884936bb49a83fa9af4d6cea1de4b96931103
juneon <tly013@naver.com>
2022-11-22 11:48:20 +0900

    refactor: Entity 클래스 추가

---
+++ src/main/java/com/project/Projectwo/Entity/Member.java
@@ -0,0 +1,47 @@
+package com.project.Projectwo.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	@Column(unique = true, length = 50)
+	private String member_id;
+	
+	@NotNull
+	private String member_pw;
+	
+	@NotNull
+	@Column(unique = true, length = 150)
+	private String email;
+	
+	@NotNull
+	@Column(length = 30)
+	private String name;
+	
+	private int age;
+	
+	@Column(length = 200)
+	private String address;
+	
+	@Column(length = 50)
+	private String tel;
+	
+	private int auth;
+
+}
