b8fec501ad7ea919918276ef492d474793738162
juneon <tly013@naver.com>
2022-11-22 16:11:43 +0900

    docs: Service&ETC 주석 추가

--- src/main/java/com/project/Projectwo/Service/AcademyService.java
+++ src/main/java/com/project/Projectwo/Service/AcademyService.java
@@ -4,5 +4,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AcademyService {
-
+	
+	/*
+	관리자
+	학원정보 수정
+	전체 공지 작성/수정
+	강의실 CRUD
+	강의 리스트 CRUD
+	강의 CRUD
+	강의 시간표 CRUD
+	선생님 관리
+	수강생 관리
+	등 ?
+	 */
 }
