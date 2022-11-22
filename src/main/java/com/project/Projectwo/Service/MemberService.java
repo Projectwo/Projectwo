ba54d59d64c785b1e4064d3de22cde642dc54557
juneon <tly013@naver.com>
2022-11-22 16:27:09 +0900

    feat: dev 병합

--- src/main/java/com/project/Projectwo/Service/MemberService.java
+++ src/main/java/com/project/Projectwo/Service/MemberService.java
@@ -4,5 +4,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-
+	
+	/*
+	 공통
+	 회원가입
+	 */
+	
+	/*
+	학생
+	내 강의 목록
+	강의 시간표 출력
+	강의 게시판 출력
+	출결 !!!!!!! 제일 중요함 따로 설명 필요
+	 */
+	
+	/*
+	선생님
+	내 수업 목록
+	강의 시간표 출력
+	강의 게시판 출력
+	공지글 작성/수정
+	 */
 }
