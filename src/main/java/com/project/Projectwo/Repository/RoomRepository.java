1ef8d105ed9fd7290efcd96d174424d2d4292dfc
juneon <tly013@naver.com>
2022-11-22 12:13:29 +0900

    refactor: Repository 인터페이스 추가

---
+++ src/main/java/com/project/Projectwo/Repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.project.Projectwo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.Projectwo.Entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+}
