3760672b7e880b887d7a8cf5c70ab435f7fa32f1
juneon <tly013@naver.com>
2022-11-23 12:44:59 +0900

    feat: 회원가입 메서드 작성

--- src/main/java/com/project/Projectwo/Service/MemberService.java
+++ src/main/java/com/project/Projectwo/Service/MemberService.java
@@ -1,10 +1,36 @@
 package com.project.Projectwo.Service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.project.Projectwo.Entity.Member;
+import com.project.Projectwo.Repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class MemberService {
 	
+	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	// by 안준언, 회원가입
+	public void create(String member_id, String member_pw,
+								String email, String name, int age,
+								String address, String tel) {
+		
+		Member member = new Member();
+		member.setMember_id(member_id);
+		member.setMember_pw(passwordEncoder.encode(member_pw));
+		member.setEmail(email);
+		member.setName(name);
+		member.setAge(age);
+		member.setAddress(address);
+		member.setTel(tel);
+		member.setAuth(1);
+		this.memberRepository.save(member);
+	}
 	/*
 	 공통
 	 회원가입
