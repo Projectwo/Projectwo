6b007777e78a0da5ba9f9d3f6d91802a902d948b
juneon <tly013@naver.com>
2022-11-29 08:14:27 +0900

    refactor: juneon2 브랜치로부터 병합

---
+++ src/main/java/com/project/Projectwo/Controller/BaseController.java
@@ -0,0 +1,23 @@
+package com.project.Projectwo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController {
+    
+    @RequestMapping("/")
+    public String root(){
+        return "member/login";
+    }
+
+    @RequestMapping("/student/main")
+    public String studentMain(){
+        return "member/member_main";
+    }
+
+    @RequestMapping("/academy/main")
+    public String academyMain(){
+        return "academy/academy_main";
+    }
+}
