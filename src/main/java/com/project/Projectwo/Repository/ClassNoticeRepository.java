f7712a188534014e26e02d6d1da693d154b5a092
juneon <tly013@naver.com>
2022-11-28 17:33:08 +0900

    연습커밋이예요

--- src/main/java/com/project/Projectwo/Repository/ClassNoticeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.Projectwo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.Projectwo.Entity.ClassNotice;
-
-public interface ClassNoticeRepository extends JpaRepository<ClassNotice, Integer> {
-
-}
--- src/main/java/com/project/Projectwo/Repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.Projectwo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.Projectwo.Entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-
-}
