0d9f146cf41fb21756d685dd2de89fe02947fbf1
m5kt4nx <m5kt4nx@gmail.com>
2022-12-09 14:14:55 +0900

    refactor: lectureDetail 내 academy notice 및 lecture list 모델 추가

--- src/main/java/com/project/Projectwo/Controller/BaseController.java
+++ src/main/java/com/project/Projectwo/Controller/BaseController.java
@@ -95,43 +95,62 @@ public class BaseController {
     	}
     }
     
-    @GetMapping("/signup")
-	public String signup(MemberCreateForm memberCreateForm) {
-		return "signup_form";
-	}
+    // @GetMapping("/signup")
+	// public String signup(MemberCreateForm memberCreateForm) {
+	// 	return "signup_form";
+	// }
 	
-	public String signup(@Valid MemberCreateForm memberCreateForm,
-							BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
-			return "signup_form";
-		}
+	// public String signup(@Valid MemberCreateForm memberCreateForm,
+	// 						BindingResult bindingResult) {
+	// 	if(bindingResult.hasErrors()) {
+	// 		return "signup_form";
+	// 	}
 		
-		if(!memberCreateForm.getPassword1().equals(memberCreateForm.getPassword2())) {
-			bindingResult.rejectValue("password2", "passwordIncorrect",
-										"2개의 비밀번호가 일치하지 않습니다.");
-			return "signup_form";
-		}
+	// 	if(!memberCreateForm.getPassword1().equals(memberCreateForm.getPassword2())) {
+	// 		bindingResult.rejectValue("password2", "passwordIncorrect",
+	// 									"2개의 비밀번호가 일치하지 않습니다.");
+	// 		return "signup_form";
+	// 	}
 		
-		try {
-			academyService.createMember(memberCreateForm.getIdentity(), memberCreateForm.getPassword1(),
-					memberCreateForm.getName(), memberCreateForm.getEmail(),
-					memberCreateForm.getBirth_date(), memberCreateForm.getAddress(),
-					memberCreateForm.getTel());
-		} catch(DataIntegrityViolationException e) {
-			e.printStackTrace();
-			bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
-			return "signup_form";
-		} catch(Exception e) {
-			e.printStackTrace();
-			bindingResult.reject("signupFailed", e.getMessage());
-			return "signup_form";
-		}
+	// 	try {
+	// 		academyService.createMember(memberCreateForm.getIdentity(), memberCreateForm.getPassword1(),
+	// 				memberCreateForm.getName(), memberCreateForm.getEmail(),
+	// 				memberCreateForm.getBirth_date(), memberCreateForm.getAddress(),
+	// 				memberCreateForm.getTel());
+	// 	} catch(DataIntegrityViolationException e) {
+	// 		e.printStackTrace();
+	// 		bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+	// 		return "signup_form";
+	// 	} catch(Exception e) {
+	// 		e.printStackTrace();
+	// 		bindingResult.reject("signupFailed", e.getMessage());
+	// 		return "signup_form";
+	// 	}
 		
-		return "redirect:/";
-	}
+	// 	return "redirect:/";
+	// }
 
 	@RequestMapping("/main/lecture/detail")
-	public String lectureDetail(){
+	public String lectureDetail(Principal principal, Model model){
+
+		Member member = this.memberService.getMember(principal.getName());
+
+		List<AcademyNotice> academyNotices = academyService.getAllAcademyNotice();
+		model.addAttribute("academyNotices", academyNotices);
+
+		if(member.getRole().equals("student")) {
+			List<Student> student = studentRepository.findByStudent(member);
+			List<Attendance> attendanceList= student.get(classCnt).getAttendanceList();
+			List<Student> studentClassList = member.getStudentClassList();
+			model.addAttribute("classList", studentClassList);
+			model.addAttribute("classCnt", classCnt);
+			model.addAttribute("attendanceList", attendanceList);
+			
+		}else if(member.getRole().equals("teacher")){
+			List<Teacher> teacherClassList = member.getTeacherClassList();
+			model.addAttribute("classList", teacherClassList);
+		}
+
 		return "lecture/lecture_detail";
 	}
 
