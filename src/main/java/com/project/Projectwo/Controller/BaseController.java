7e51341e6fe5e5c8790a96bfeaa57b873fef1dc6
m5kt4nx <m5kt4nx@gmail.com>
2022-12-09 15:59:37 +0900

    refactor: 매핑 추가 및 수정

--- src/main/java/com/project/Projectwo/Controller/BaseController.java
+++ src/main/java/com/project/Projectwo/Controller/BaseController.java
@@ -63,40 +63,40 @@ public class BaseController {
     	}
     }
     
-    @GetMapping("/signup")
-	public String signup(MemberCreateForm memberCreateForm) {
-		return "signup_form";
-	}
+    // @GetMapping("/signup")
+	// public String signup(MemberCreateForm memberCreateForm) {
+	// 	return "signup_form";
+	// }
 	
-	public String signup(@Valid MemberCreateForm memberCreateForm,
-							BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
-			return "signup_form";
-		}
+	// public String signup(@Valid MemberCreateForm memberCreateForm,
+	// 						BindingResult bindingResult) {
+	// 	if(bindingResult.hasErrors()) {
+	// 		return "signup_form";
+	// 	}
 		
-		if(!memberCreateForm.getPassword1().equals(memberCreateForm.getPassword2())) {
-			bindingResult.rejectValue("password2", "passwordIncorrect",
-										"2개의 비밀번호가 일치하지 않습니다.");
-			return "signup_form";
-		}
+	// 	if(!memberCreateForm.getPassword1().equals(memberCreateForm.getPassword2())) {
+	// 		bindingResult.rejectValue("password2", "passwordIncorrect",
+	// 									"2개의 비밀번호가 일치하지 않습니다.");
+	// 		return "signup_form";
+	// 	}
 		
-		try {
-			academyService.createMember(memberCreateForm.getIdentity(), memberCreateForm.getPassword1(),
-					memberCreateForm.getName(), memberCreateForm.getEmail(),
-					memberCreateForm.getBirth_date(), memberCreateForm.getAddress(),
-					memberCreateForm.getTel());
-		} catch(DataIntegrityViolationException e) {
-			e.printStackTrace();
-			bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
-			return "signup_form";
-		} catch(Exception e) {
-			e.printStackTrace();
-			bindingResult.reject("signupFailed", e.getMessage());
-			return "signup_form";
-		}
+	// 	try {
+	// 		academyService.createMember(memberCreateForm.getIdentity(), memberCreateForm.getPassword1(),
+	// 				memberCreateForm.getName(), memberCreateForm.getEmail(),
+	// 				memberCreateForm.getBirth_date(), memberCreateForm.getAddress(),
+	// 				memberCreateForm.getTel());
+	// 	} catch(DataIntegrityViolationException e) {
+	// 		e.printStackTrace();
+	// 		bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+	// 		return "signup_form";
+	// 	} catch(Exception e) {
+	// 		e.printStackTrace();
+	// 		bindingResult.reject("signupFailed", e.getMessage());
+	// 		return "signup_form";
+	// 	}
 		
-		return "redirect:/";
-	}
+	// 	return "redirect:/";
+	// }
     
 	@RequestMapping("/main/lecture/detail")
 	public String lectureDetail(){
